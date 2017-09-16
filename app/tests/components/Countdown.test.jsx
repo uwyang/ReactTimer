01a2f374b69a89660df9dd03d7a3cabd342ec551
penpen <uwyang@gmail.com>
2017-09-16 01:10:07 -0400

    adding countdown test

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,58 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils  = require('react-addons-test-utils');
+var $ = require('jquery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  //done is used to by pass async nature of the setimeout test
+  it('valid time passing', (done)=> {
+    var spy = expect.createSpy();
+    var countdown = TestUtils.renderIntoDocument(
+      <Countdown />
+    );
+    countdown.handleSetCountdown(3);
+    var $el = $(ReactDOM.findDOMNode(countdown));
+
+    expect(countdown.state.count).toBe(3);
+    expect(countdown.state.countdownStatus).toBe('started');
+
+    //use setTimeout will be async.
+    //mocha doens't like this.
+
+    setTimeout(()=> {
+      expect(countdown.state.count).toBe(2);
+      done();
+    }, 1001);
+
+  });
+
+  //done is used to by pass async nature of the setimeout test
+  it('countdown never goes below 0', (done)=> {
+    var spy = expect.createSpy();
+    var countdown = TestUtils.renderIntoDocument(
+      <Countdown />
+    );
+    countdown.handleSetCountdown(1);
+    var $el = $(ReactDOM.findDOMNode(countdown));
+
+    expect(countdown.state.count).toBe(1);
+    expect(countdown.state.countdownStatus).toBe('started');
+
+    //use setTimeout will be async.
+    //mocha doens't like this.
+
+    setTimeout(()=> {
+      expect(countdown.state.count).toBe(0);
+      done();
+    }, 2001);
+
+  });
+
+});
